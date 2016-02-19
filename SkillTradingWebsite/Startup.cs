a4d7df5340dd094186d806089e0a3f3b183c7654
bfmv991 <eskaoneofficial@gmail.com>
2016-02-19 16:02:03 +0100

    Start

---
+++ SkillTradingWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkillTradingWebsite.Startup))]
+namespace SkillTradingWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
